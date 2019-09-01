1006d9bfb88281cfc14989a9dc0ad51a516fee8a
Thomas Sivilay <thomas.sivilay@gmail.com>
2019-09-02 07:43:49 +1000

    Added xcode project

---
+++ AppStore/AppStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStore
+//
+//  Created by Thomas Sivilay on 2/9/19.
+//  Copyright © 2019 Thomas Sivilay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
